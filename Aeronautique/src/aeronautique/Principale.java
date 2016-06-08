8aa8db4f46fc7cfac6b3537f24871aaeefae64d8
ib <ib@WINIB-UVRLHG1C6.local>
2016-06-08 12:14:39 +0200

    creation du projet aeronautique

---
+++ Aeronautique/src/aeronautique/Principale.java
@@ -0,0 +1,11 @@
+package aeronautique;
+
+public class Principale {
+
+	public static void main(String[] args) {
+		Avion avion = new Avion(1, "airbus", "rennes", 350);
+		System.out.println(avion);
+
+	}
+
+}
